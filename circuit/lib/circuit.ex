0b3832aaecfd450524394df4145931c330091f35
Brian Lubeski <bclube@gmail.com>
2019-08-01 22:57:37 -0400

    Remove boilerplate added by `mix new`

--- circuit/lib/circuit.ex
+++ circuit/lib/circuit.ex
@@ -1,18 +1,2 @@
 defmodule Circuit do
-  @moduledoc """
-  Documentation for Circuit.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Circuit.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- circuit/test/circuit_test.exs
+++ circuit/test/circuit_test.exs
@@ -1,8 +1,4 @@
 defmodule CircuitTest do
   use ExUnit.Case
   doctest Circuit
-
-  test "greets the world" do
-    assert Circuit.hello() == :world
-  end
 end
