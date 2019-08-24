e5939df7be209ab56ea527003dbba6f63211cb97
Brian Lubeski <bclube@gmail.com>
2019-08-23 23:12:38 -0400

    Add solution and tests for problem 4

---
+++ daily_coding_exercise/lib/problem_0004.ex
@@ -0,0 +1,59 @@
+defmodule Problem0004 do
+  @moduledoc """
+  Given the mapping a = 1, b = 2, ... z = 26, and an encoded message, count
+  the number of ways it can be decoded.
+
+  For example, the message '111' would give 3, since it could be decoded as
+  'aaa', 'ka', and 'ak'.
+
+  You can assume that the messages are decodable. For example, '001' is not
+  allowed.
+  """
+
+  @doc """
+  ## Examples
+
+    iex> Problem0004.solve("")
+    0
+
+    iex> Problem0004.solve("1")
+    1
+
+    iex> Problem0004.solve("26")
+    2
+
+    iex> Problem0004.solve("111")
+    3
+
+  """
+  def solve(""), do: 0
+
+  def solve(encoded) when is_binary(encoded) do
+    encoded
+    |> String.graphemes()
+    |> Stream.map(&String.to_integer/1)
+    |> Enum.reverse()
+    |> solve_impl()
+  end
+
+  defp solve_impl([d | rest]) do
+    solve_impl(rest, d, lookup_single(d), 1)
+  end
+
+  defp solve_impl([d | rest], d2, p1, p2) do
+    subresult =
+      lookup_double(d, d2) * p2 +
+        lookup_single(d) * p1
+
+    solve_impl(rest, d, subresult, p1)
+  end
+
+  defp solve_impl([], _d, p1, _p2), do: p1
+
+  defp lookup_single(d) when d > 0, do: 1
+  defp lookup_single(_), do: 0
+
+  defp lookup_double(1, _), do: 1
+  defp lookup_double(2, b) when b <= 6, do: 1
+  defp lookup_double(_, _), do: 0
+end
---
+++ daily_coding_exercise/test/problem_0004_test.exs
@@ -0,0 +1,107 @@
+defmodule Problem0004Test do
+  use ExUnit.Case, async: true
+  use PropCheck
+  doctest Problem0004
+
+  setup_all do
+    valid_codes = for n <- 1..26, do: Integer.to_string(n), into: MapSet.new()
+
+    base_case_solutions =
+      for(
+        a <- valid_codes,
+        String.length(a) == 1,
+        b <- valid_codes,
+        String.length(b) == 1,
+        do: a <> b
+      )
+      |> Stream.concat(valid_codes)
+      |> Enum.reduce(%{}, &Map.update(&2, &1, 1, fn v -> v + 1 end))
+
+    [
+      valid_codes: valid_codes,
+      base_case_solutions: base_case_solutions
+    ]
+  end
+
+  describe "#{inspect(&Problem0004.solve/1)}:" do
+    test "[base case 1] all possible digit strings with length == 1: matches computed value", %{
+      base_case_solutions: base_case_solutions
+    } do
+      for int <- 0..9, code = Integer.to_string(int) do
+        expected_result = Map.get(base_case_solutions, code, 0)
+
+        assert Problem0004.solve(code) == expected_result,
+               """
+               Failed with code: #{code}
+               Expected result: #{expected_result}
+               Actual result: #{Problem0004.solve(code)}
+               """
+      end
+    end
+
+    test "[base case 2] all possible digit strings with length == 2: matches computed value", %{
+      base_case_solutions: base_case_solutions
+    } do
+      for a <- 0..9, b <- 0..9, code = Enum.join([a, b]) do
+        expected_result = Map.get(base_case_solutions, code, 0)
+
+        assert Problem0004.solve(code) == expected_result,
+               """
+               Failed with code: #{code}
+               Expected result: #{expected_result}
+               Actual result: #{Problem0004.solve(code)}
+               """
+      end
+    end
+
+    property "[inductive case] random code with length(code) >= 3: matches inductive proof",
+             [
+               :verbose
+             ],
+             %{
+               valid_codes: valid_codes
+             } do
+      forall code <- code_with_three_or_more_characters() do
+        {_, rest1} = String.split_at(code, 1)
+        {prefix2, rest2} = String.split_at(code, 2)
+
+        subresult1 = Problem0004.solve(rest1)
+
+        subresult2 =
+          if MapSet.member?(valid_codes, prefix2),
+            do: Problem0004.solve(rest2),
+            else: 0
+
+        expected_result = subresult1 + subresult2
+
+        (Problem0004.solve(code) == expected_result)
+        |> collect({:code_length, PropTestHelpers.bucket(String.length(code), 10)})
+        |> when_fail(
+          IO.puts("""
+          Code: #{code}
+          Expected result: #{inspect(expected_result)}
+          Actual result: #{inspect(Problem0004.solve(code))}
+          """)
+        )
+      end
+    end
+  end
+
+  ###
+  ### Generators
+  ###
+  defp code_with_three_or_more_characters do
+    let(
+      {a, b, rest} <-
+        such_that(
+          {code1, code2, code_rest} <- {valid_code(), valid_code(), list(valid_code())},
+          when: code1 > 9 || code2 > 9 || length(code_rest) > 0
+        ),
+      do: Enum.join([a, b | rest])
+    )
+  end
+
+  defp valid_code do
+    choose(1, 26)
+  end
+end
